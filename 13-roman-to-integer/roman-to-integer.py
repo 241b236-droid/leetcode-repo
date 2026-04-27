42e93b2a3fb93af28db5d6bc0b8f86484f7c0b70
Shashank Singh <241b236@juetguna.in>
2026-04-27 17:20:22 +0530

    Time: 3 ms (93.49%) | Memory: 12.3 MB (91.93%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def romanToInt(self, s):
+        values = {"I": 1, "V": 5, "X": 10, "L": 50,
+                  "C": 100, "D": 500, "M": 1000}
+
+        result = 0
+        n = len(s)
+
+        for i in range(n):
+            current = values[s[i]]
+            if i < n - 1 and current < values[s[i + 1]]:
+                result -= current
+            else:
+                result += current
+
+        return result
